88ac73c26d1837c5a13b324c09823122382d2876
dvfcfbgv <ian21116938@tas.tw>
2019-01-07 02:53:04 +0800

    changed main folder to 'main_control'

--- main_control/wsgi.py
+++ main_control/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djgraph project.
+WSGI config for the Django backend.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djgraph.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_control.settings')
 
 application = get_wsgi_application()
