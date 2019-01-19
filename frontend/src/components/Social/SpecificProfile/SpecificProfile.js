0edf056b1c15249a570778a592a68236da782c1d
dvfcfbgv <21ianh1@tas.tw>
2019-01-19 23:38:22 +0800

    added some fields into profile page

--- frontend/src/components/Social/SpecificProfile/SpecificProfile.js
+++ frontend/src/components/Social/SpecificProfile/SpecificProfile.js
@@ -7,21 +7,25 @@ export default props => {
 	let {
 		firstName,
 		lastName,
+		isStaff,
 		image,
 		username,
 		email,
 		dateOfBirth,
 		lastLogin,
-		isActive
+		isActive,
+		bio
 	} = props.user
 	return (
 		<div>
 			<img src={image} alt="Profile" />
 			<h1>{isActive}</h1>
+			<h1>{isStaff}</h1>
 			<h1>{`${firstName} ${lastName}`}</h1>
 			<h1>{username}</h1>
 			<h1>{email}</h1>
 			<h1>{dateOfBirth}</h1>
+			<div>{bio}</div>
 			<h1>{lastLogin}</h1>
 		</div>
 	)
